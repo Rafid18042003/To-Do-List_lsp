d3f2b4558abed381702f25fc24111118328503fd
Rafid18042003 <rafid.rizkulloh@widyatama.ac.id>
2025-06-12 15:37:45 +0700

    Update data.php

--- data.php
+++ data.php
@@ -1,10 +1,9 @@
 <?php
-// Mulai session jika belum aktif
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-// Inisialisasi data tugas awal jika belum ada di session
 if (!isset($_SESSION['tasks'])) {
     $_SESSION['tasks'] = [
         ['judul' => 'Membuat to-do list dengan PHP', 'status' => 'selesai'],
